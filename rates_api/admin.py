2024d4ae80dafd3e727cd17bcb792fda98721995
Hamza Ishaq <hamza.net@live.com>
2019-06-17 05:32:36 +0500

    fixtures added

--- rates_api/admin.py
+++ rates_api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Region, Port, Price
+
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url_slug', 'parent',)
+    list_display_links = ('id', 'name',)
+    raw_id_fields = ('parent',)
+    exclude = ('slug',)
+    search_fields = ('name',)
+
+
+class PortAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'name', 'region',)
+    list_display_links = ('id', 'code',)
+    raw_id_fields = ('region',)
+    search_fields = ('code', 'name',)
+
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'origin', 'destination', 'day', 'price')
+    raw_id_fields = ('origin', 'destination')
+    search_fields = ('origin', 'destination', 'day')
+
+
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Port, PortAdmin)
+admin.site.register(Price, PriceAdmin)
--- rates_api/models.py
+++ rates_api/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils.text import slugify
 
-from .constants import DAY_CHOICES
-
 
 class Region(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=75)
     slug = models.SlugField()
     parent = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def url_slug(self):
+        return '{}-{}'.format(self.slug, self.pk)
+
+    def __str__(self):
+        return self.name
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         self.slug = slugify(self.name)
@@ -16,13 +21,16 @@ class Region(models.Model):
 
 class Port(models.Model):
     # 5-character Port Code
-    code = models.CharField(max_length=5)
-    name = models.TextField()
+    code = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=75)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.code)
 
 
 class Price(models.Model):
     origin = models.ForeignKey(Port, on_delete=models.CASCADE, related_name='price_origin')
     destination = models.ForeignKey(Port, on_delete=models.CASCADE, related_name='price_destination')
-    day = models.IntegerField(choices=DAY_CHOICES)
+    day = models.DateField()
     price = models.DecimalField(decimal_places=2, max_digits=20)  # price is stored in USD
